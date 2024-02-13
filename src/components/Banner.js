ea82fb2887d4b68991ce809453d1307837e9ebf5
Eucotopia <3499508634@qq.com>
2024-02-13 22:42:44 +0800

    inital

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import Image from '../assets/avatar.svg'
+import {FaGithub, FaYoutube, FaDribble} from 'react-icons/fa'
+import {TypeAnimation, typeAnimation} from 'react-type-animation'
+import {motion} from 'framer-motion'
+import {fadeIn} from "../variants";
 
 const Banner = () => {
-  return <div className={"section"} id={'home'}>Banner</div>;
+    return <section className={"min-h-[85vh] lg:min-h-[78vh] flex items-center"} id={'home'}>
+        <div className="container mx-auto">
+            <div className={'flex flex-col gap-y-8 lg:flex-row items-center'}>
+                <div className={'flex-1 text-center font-secondary lg:text-left'}>
+                    <h1 className={'text-[55px] font-bold leading-[0.8] lg:text-[110px]'}>BEN<span>AIDEN</span></h1>
+                    <div
+                        className={'mb-5 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'}>
+                        <span className={'text-white mr-4'}>I am a </span>
+                        <TypeAnimation speed={50} className={"text-accent"} wrapper={'span'} repeat={Infinity}
+                                       sequence={['Developer', 2000, 'Designer', 2000, 'Youtuber', 2000]}/>
+                    </div>
+                    <p className={'mb-8 max-w-lg mx-auto lg:mx-0'}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Unde,
+                        numquam. Harum deleniti id sapiente
+                    </p>
+                    <div className={'flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'}>
+                        <button className={'btn btn-lg'}>Contact Me</button>
+                        <a href={"#"} className={'text-gradient btn-link'}>My portfolio</a>
+                    </div>
+                    <div className={'flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'}>
+                        <a href={"#"}
+                        >
+                            <FaYoutube/>
+                        </a>
+                        <a href={"#"}
+                        >
+                            <FaYoutube/>
+                        </a>
+                        <a href={"#"}
+                        >
+                            <FaYoutube/>
+                        </a>
+                        <a href={"#"}
+                        >
+                            <FaYoutube/>
+                        </a>
+                    </div>
+                </div>
+
+                <div className={'hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'}>
+                    <img src={Image} align={''}/>
+                </div>
+            </div>
+        </div>
+    </section>;
 };
 
 export default Banner;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import {BiHomeAlt, BiLink, BiUser} from "react-icons/bi";
+import {BiHomeAlt, BiUser} from "react-icons/bi";
 import {Link} from "react-scroll"
 import {BsBriefcase, BsChatSquareText, BsClipboardData} from "react-icons/bs";
 
 const Nav = () => {
-  return <nav className={'fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'}>
-    <div className="container mx-auto">
-      <div className={'w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50'}>
-        <Link to={'home'} className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
-          <BiHomeAlt/>
-        </Link>
-        <Link to={'about'} className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
-          <BiUser/>
-        </Link>
-        <Link to="services" className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
-          <BsClipboardData/>
-        </Link>
-        <Link to="work" className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
-          <BsBriefcase/>
-        </Link>
-        <Link to="contact" className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
-          <BsChatSquareText/>
-        </Link>
-      </div>
-    </div>
-  </nav>;
+    return <nav className={'fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'}>
+        <div className="container mx-auto">
+            <div
+                className={'w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50'}>
+                <Link
+                    offset={-200}
+                    activeClass={'active'}
+                    smooth={true}
+                    spy={true}
+                    to={'home'} className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
+                    <BiHomeAlt/>
+                </Link>
+                <Link
+                    activeClass={'active'}
+                    smooth={true}
+                    spy={true}
+                    to={'about'} className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
+                    <BiUser/>
+                </Link>
+                <Link
+                    activeClass={'active'}
+                    smooth={true}
+                    spy={true}
+                    to="services" className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
+                    <BsClipboardData/>
+                </Link>
+                <Link
+                    activeClass={'active'}
+                    smooth={true}
+                    spy={true}
+                    to="work" className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
+                    <BsBriefcase/>
+                </Link>
+                <Link
+                    activeClass={'active'}
+                    smooth={true}
+                    spy={true}
+                    to="contact" className={'cursor-pointer w-[60px] h-[60px] flex items-center'}>
+                    <BsChatSquareText/>
+                </Link>
+            </div>
+        </div>
+    </nav>;
 };
 
 export default Nav;
